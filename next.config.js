aa9bc765c4be024cdde1e280716a41bfa4960c15
boming.ying <boming.ying@heliang.cc>
2023-03-31 15:41:56 +0800

    ✨ feat: update env.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
+  },
+  env: {
+    APP_ENV: process.env.APP_ENV
   }
   // sassOptions: {
   //   includePaths: [path.join(__dirname, 'styles')],
